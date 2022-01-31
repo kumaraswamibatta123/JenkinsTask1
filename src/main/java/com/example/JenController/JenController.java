fbe3faea0667e9436a940df9d76bde75e5198847
reactvehiclerentalsystem <kumaraswamibatta123@gmail.com>
2022-01-31 13:19:43 +0530

    ComiiTED

---
+++ src/main/java/com/example/JenController/JenController.java
@@ -0,0 +1,14 @@
+package com.example.JenController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class JenController {
+	
+	@GetMapping("/")
+	public String hele() {
+		System.out.println("Helo JEn");
+		return "HElo JEn";
+	}
+
+}
